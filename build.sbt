590345d1b4df920be327e5b833ad138c4b53aba4
enzo <fabre.enzoj@gmail.com>
2018-10-23 12:29:34 +0200

    test debut spark

--- build.sbt
+++ build.sbt
@@ -4,5 +4,18 @@ version := "0.1"
 
 scalaVersion := "2.11.8"
 
+val sparkVersion = "2.3.2"
+
+resolvers ++= Seq(
+  "apache-snapshots" at "http://repository.apache.org/snapshots/"
+)
+
 // https://mvnrepository.com/artifact/org.apache.spark/spark-core
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % sparkVersion,
+  "org.apache.spark" %% "spark-streaming" % sparkVersion,
+  "org.apache.spark" %% "spark-hive" % sparkVersion,
+  "mysql" % "mysql-connector-java" % "5.1.6"
+)
--- src/main/scala/SparkAPI.scala
+++ src/main/scala/SparkAPI.scala
@@ -1,12 +1,12 @@
 import org.apache.spark._
 import org.apache.log4j._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SparkContext
 
 /**
   * Created by toddmcgrath on 6/15/16.
   */
-object SparkAPI {
-
-  def main(args: Array[String]) {
+object SparkAPI extends App {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
     val sc = new SparkContext("local[*]", "ScalaProcess")
@@ -18,6 +18,11 @@ object SparkAPI {
     println(output(1))
     println(output(3))
 
-  }
+    val spark = SparkSession.builder().getOrCreate()
+    val data = spark.read.option("header", "true").option("inferSchema", "true").format("json")
+          .load("public/data-studnets.json")
+    data.printSchema()
+
+
 
 }
