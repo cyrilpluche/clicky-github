fe360eabe38c423a94e990d28295a566182ac771
Unknown <alex.bottero@hotmail.fr>
2018-10-23 12:49:14 +0200

    struct project

--- src/main/scala/SparkAPI.scala
+++ src/main/scala/SparkAPI.scala
@@ -1,7 +1,6 @@
 import org.apache.spark._
 import org.apache.log4j._
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.SparkContext
 
 /**
   * Created by toddmcgrath on 6/15/16.
@@ -9,20 +8,12 @@ import org.apache.spark.sql.SparkContext
 object SparkAPI extends App {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
-    val sc = new SparkContext("local[*]", "ScalaProcess")
-    val lines = sc.textFile("public/data-students.json")
-    val output = lines.take(10)
-    sc.stop()
 
-    println(output(0))
-    println(output(1))
-    println(output(3))
-
-    val spark = SparkSession.builder().getOrCreate()
+    val spark = SparkSession.builder().config("spark.master","local").getOrCreate()
     val data = spark.read.option("header", "true").option("inferSchema", "true").format("json")
-          .load("public/data-studnets.json")
+          .load("public/data-students.json")
     data.printSchema()
-
+    spark.stop()
 
 
 }
